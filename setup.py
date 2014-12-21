fc7667b5db8b08ca0459d52987a704cd530107e2
derfenix <derfenix@gmail.com>
2014-12-21 13:58:57 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,21 @@ from distutils.core import setup
 
 setup(
     name='crosservice',
-    version='0.2.0',
+    version='0.2.1',
     packages=['crosservice'],
-    url='http://',
     install_requires=open('requirements.txt').read(),
     license='GPL',
     author='derfenix',
     author_email='derfenix@gmail.com',
-    description='Cross-service comunication python library'
+    description='Cross-service comunication python library',
+    url='https://github.com/derfenix/crosservice',
+    download_url='https://github.com/derfenix/crosservice/archive/v.0.2.1'
+                 '.tar.gz',
+    keywords=['communication', 'gevent', 'socket'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+    ],
 )
